3dfabdacb5b47a043847448cfc9ee3173239ed57
lmn165 <lastalking@naver.com>
2021-08-25 10:21:01 +0900

    Aug 25 reserved word

---
+++ src/modern-javascript/chap-01-basic/basic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+var man = {} // es5
+let tom = 'Tom' // es6에서 variable
+const james = 'James' // es6 에서 constant
+
+const Basic = () => {
+
+    return(<></>)
+}
+
+export default Basic
