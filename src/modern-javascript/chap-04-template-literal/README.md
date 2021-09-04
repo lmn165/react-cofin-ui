f829b412a27820e3f522a74fca46502f2532165f
lmn165 <lastalking@naver.com>
2021-09-04 16:57:15 +0900

    Sept 04 update chap 04

--- src/modern-javascript/chap-04-template-literal/README.md
+++ src/modern-javascript/chap-04-template-literal/README.md
@@ -1,3 +1,10 @@
-# Template Literal
+## Template Literal
 #### ES5 에서 Template String 이 ES6 에서 Template Literal 로 변환되었다.
-#### Template Literal 에서는 백틱(`) 을 사용한다.
++ Template Literal 에서는 백틱(`) 을 사용한다.
++ 마찬가지로 표현식을 삽입하는 경우에도 백틱(`) 을 사용한다.
+```
+let a = 1;
+let b = 10;
+console.log(`1 * 10 is ${a * b}`);
+// 1 * 10 is 10
+```
--- src/modern-javascript/chap-04-template-literal/TemplateLiteral.js
+++ src/modern-javascript/chap-04-template-literal/TemplateLiteral.js
@@ -2,6 +2,35 @@ import React from 'react'
 
 const TemplateLiteral = () => {
 
+    // ES5 템플릿 스트링(template string)
+    const templateString = () => {
+        const name = "Alberto";
+        const greeting = 'Hello my name is ' + name;
+        
+        console.log(greeting);
+    }
+    
+    // ES6 템플릿 리터럴(tmeplate literal)
+    const TemplateLiteral = () => {
+        const name = "Alberto";
+        const greeting = `Hello my name is ${name}`;
+
+        console.log(greeting);
+    }
+
+
+    /**
+     * text1 : ES5 에서 여러 줄 문자열 생성 방식
+     * text2 : ES6 에서 여러 줄 문자열 생성 방식
+     */
+    let text1 = "hello, \
+    my name is Alberto \
+    how are you?\ ";
+
+    let text2 = `hello,
+    my name is Alberto
+    how are you?`;
+
     return (<></>)
 }
 
