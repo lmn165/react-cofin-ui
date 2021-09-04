9f4ceafb294ac0ef18fe55c3b3e547efc3fc8d31
lmn165 <lastalking@naver.com>
2021-09-04 16:38:58 +0900

    Sept 04 update chap 03

--- src/modern-javascript/chap-03-arguments/Arguments.js
+++ src/modern-javascript/chap-03-arguments/Arguments.js
@@ -2,6 +2,18 @@ import React from 'react'
 
 const Arguments = () => {
 
+    /**
+     * = {} 를 추가해야 인수를 기본적으로 객체로 설정한다.
+     * = {} 가 없다면, 인수로 무엇을 전달하든 아래 오류 문구가 출력된다.
+     * cannot read property `total` of 'undefined' or 'null'
+     */
+    function calculatePrice({total = 0, tax = 0.1, tip = 0.05,} = {}){
+        return total + (total * tax) + (total * tip);
+    }
+
+    const bill = calculatePrice({tip: 0.15, total: 150});
+    // 187.5
+
     return (<></>)
 }
 
